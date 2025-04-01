4559b8f15f923c21d718fc8a2820ae7f97a7347b
Deeksha Chauhan <deeksha7chauhan@gmail.com>
2025-04-01 14:25:42 -0700

    Update Basic Aggregate Functions.sql
    2 solved aggregate questions

--- Basic Aggregate Functions.sql
+++ Basic Aggregate Functions.sql
@@ -18,3 +18,20 @@ select p.project_id,  ROUND(AVG(e.experience_years),2) AS average_years FROM Pro
 #1633. Percentage of Users Attended a Contest
 #Write a solution to find the percentage of the users registered in each contest rounded to two decimals. Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
 select contest_id, round(count(distinct user_id)*100/(select count(user_id) from Users),2)  as percentage from Register group by contest_id order by percentage desc, contest_id;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------
+#1211. Queries Quality and Percentage
+#Write a solution to find each query_name, the quality and poor_query_percentage.Both quality and poor_query_percentage should be rounded to 2 decimal places.
+SELECT query_name, ROUND(AVG(rating/position),2) AS quality, ROUND((SUM(rating<3)/(COUNT(*)))*100, 2)  AS poor_query_percentage FROM Queries group by query_name;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------
+#1193. Monthly Transactions I
+#Write an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
+# Write your MySQL query statement below
+select DATE_FORMAT(trans_date, '%Y-%m') as month, country, count(trans_date) as trans_count,  sum(if(state='approved', 1, 0)) as approved_count, sum(amount) as trans_total_amount,  sum(if(state='approved', amount, 0)) as approved_total_amount
+from transactions group by month, country;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------
+#1174. Immediate Food Delivery II
+#Write a solution to find the percentage of immediate orders in the first orders of all customers, rounded to 2 decimal places.
+  
