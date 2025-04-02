013d5626b6f378d3b065ab27cc655bfc44b07da1
Deeksha Chauhan <deeksha7chauhan@gmail.com>
2025-04-01 23:50:09 -0700

    Update Basic Aggregate Functions.sql

--- Basic Aggregate Functions.sql
+++ Basic Aggregate Functions.sql
@@ -32,6 +32,12 @@ select LEFT(trans_date, 7) as month, country, count(trans_date) as trans_count, 
 from transactions group by month, country;
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------
+#550. Game Play Analysis IV
+#Write a solution to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places.
+SELECT ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) as fraction FROM Activity WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
+IN (SELECT player_id, MIN(event_date) AS first_login FROM ACTIVITY GROUP BY player_id);
+
+----------------------------------------------------------------------------------------------------------------------------------------------------
 #1174. Immediate Food Delivery II
 #Write a solution to find the percentage of immediate orders in the first orders of all customers, rounded to 2 decimal places.
   
